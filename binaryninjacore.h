6a3923153aa73de3385315ce6e42d6f1f1e9d369
Glenn Smith <glenn@vector35.com>
2020-11-03 15:33:25 -0500

    KVS constructor

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -2713,6 +2713,7 @@ __attribute__ ((format (printf, 1, 2)))
 	BINARYNINJACOREAPI BNDatabase* BNGetFileMetadataDatabase(BNFileMetadata* file);
 
 	// Key value store
+	BINARYNINJACOREAPI BNKeyValueStore* BNCreateKeyValueStore(void);
 	BINARYNINJACOREAPI BNKeyValueStore* BNNewKeyValueStoreReference(BNKeyValueStore* store);
 	BINARYNINJACOREAPI void BNFreeKeyValueStore(BNKeyValueStore* store);
 
